5222471dab7104374530f0d5cf0978c38d6cf7c1
AlejandroLucas161 <alejandrolucas161@gmail.com>
2021-12-02 14:25:12 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '36876a1d-2bac-429d-8639-dfc782a3a884';
+export const MODAL_CLOSE_SEC = 2.5;
